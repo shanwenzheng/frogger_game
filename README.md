98dd078d695d0ea94c589d3047bb345eb34dc4dc
scyws1 <scyws1@exmal.nottingham.ac.uk>
2019-11-26 16:00:02 +0000

    Split GameView from main class

--- README.md
+++ README.md
@@ -1,3 +1,15 @@
 # <img src="http://b-ssl.duitang.com/uploads/item/201801/25/20180125131358_tVkML.thumb.1000_0.jpeg" alt="pacman logo" width="50"/> Frogger
 
-> This is a Frogger game project written in JavaFX.
+> This is a Frogger game project written in JavaFX.<br>
+
+## Refactor Log
+
+#### ① Model Refactor
+
+#### ② View Refactor
+
+* Split `gameView` from main class
+
+#### ③ Util Refactor
+
+#### ④ Controller Refactor
---
+++ src/main/java/frogger/GameView.java
@@ -0,0 +1,58 @@
+package frogger;
+
+public class GameView {
+	private MyStage background;
+	private Animal animal;
+	
+	public GameView() {
+		background = new MyStage();
+		animal = new Animal("images/froggerUp.png");
+	}
+	
+	public MyStage getBackground() {
+		return background;
+	}
+
+	public Animal getAnimal() {
+		return animal;
+	}
+
+	public void drawMap() {
+		BackgroundImage froggerback = new BackgroundImage("images/iKogsKW.png");
+	    
+		background.add(froggerback);
+		
+		background.add(new Log("images/log3.png", 150, 0, 166, 0.75));
+		background.add(new Log("images/log3.png", 150, 220, 166, 0.75));
+		background.add(new Log("images/log3.png", 150, 440, 166, 0.75));
+		background.add(new Log("images/logs.png", 300, 0, 276, -2));
+		background.add(new Log("images/logs.png", 300, 400, 276, -2));
+		background.add(new Log("images/log3.png", 150, 50, 329, 0.75));
+		background.add(new Log("images/log3.png", 150, 270, 329, 0.75));
+		background.add(new Log("images/log3.png", 150, 490, 329, 0.75));
+		
+		background.add(new Turtle(500, 376, -1, 130, 130));
+		background.add(new Turtle(300, 376, -1, 130, 130));
+		background.add(new WetTurtle(700, 376, -1, 130, 130));
+		background.add(new WetTurtle(600, 217, -1, 130, 130));
+		background.add(new WetTurtle(400, 217, -1, 130, 130));
+		background.add(new WetTurtle(200, 217, -1, 130, 130));
+		background.add(new End(13,96));
+		background.add(new End(141,96));
+		background.add(new End(141 + 141-13,96));
+		background.add(new End(141 + 141-13+141-13+1,96));
+		background.add(new End(141 + 141-13+141-13+141-13+3,96));
+		background.add(animal);
+		background.add(new Obstacle("images/truck1"+"Right.png", 0, 649, 1, 120, 120));
+		background.add(new Obstacle("images/truck1"+"Right.png", 300, 649, 1, 120, 120));
+		background.add(new Obstacle("images/truck1"+"Right.png", 600, 649, 1, 120, 120));
+		background.add(new Obstacle("images/car1Left.png", 100, 597, -1, 50, 50));
+		background.add(new Obstacle("images/car1Left.png", 250, 597, -1, 50, 50));
+		background.add(new Obstacle("images/car1Left.png", 400, 597, -1, 50, 50));
+		background.add(new Obstacle("images/car1Left.png", 550, 597, -1, 50, 50));
+		background.add(new Obstacle("images/truck2Right.png", 0, 540, 1, 200, 200));
+		background.add(new Obstacle("images/truck2Right.png", 500, 540, 1, 200, 200));
+		background.add(new Obstacle("images/car1Left.png", 500, 490, -5, 50, 50));
+		background.add(new Digit(0, 30, 360, 25));
+	}
+}
