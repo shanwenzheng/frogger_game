4c2806c29db08766d9761350456ef027c11205d8
scyws1 <scyws1@exmal.nottingham.ac.uk>
2019-11-27 22:26:23 +0000

    Refactor Turtle class

--- src/main/java/model/Turtle.java
+++ src/main/java/model/Turtle.java
@@ -1,45 +1,30 @@
 package model;
 
+import java.util.ArrayList;
 
-import frogger.Main;
 import javafx.scene.image.Image;
 
 public class Turtle extends MovableActor{
-	Image turtle1;
-	Image turtle2;
-	Image turtle3;
-	private int speed;
-	int i = 1;
-	boolean bool = true;
+	private ArrayList<Image> turtleImages;	
+	
+	public Turtle(int size, double  xpos, double  ypos, double  s) {
+		super(xpos, ypos, s);
+		turtleImages = new ArrayList<Image>() {{
+			add(new Image("file:resources/images/TurtleAnimation2.png", size, size, true, true));
+			add(new Image("file:resources/images/TurtleAnimation1.png", size, size, true, true));
+			add(new Image("file:resources/images/TurtleAnimation3.png", size, size, true, true));
+		}};
+		setImage(turtleImages.get(0));
+	}
+	
 	@Override
 	public void act(long now) {
-
-				if (now/900000000  % 3 ==0) {
-					setImage(turtle2);
-					
-				}
-				else if (now/900000000 % 3 == 1) {
-					setImage(turtle1);
-					
-				}
-				else if (now/900000000 %3 == 2) {
-					setImage(turtle3);
-					
-				}
-			
-		move(speed , 0);
-		if (getX() > 600 && speed>0)
-			setX(-200);
-		if (getX() < -75 && speed<0)
-			setX(600);
+		changeTurtleImage((int) (now/900000000  % 3));
+		move(getSpeed(), 0);
+		checkWall(getSpeed(),-75,-200,600);
 	}
-	public Turtle(int size, int xpos, int ypos, int s) {
-		turtle1 = new Image(Main.class.getResourceAsStream("images/TurtleAnimation1.png"), size, size, true, true);
-		turtle2 = new Image(Main.class.getResourceAsStream("images/TurtleAnimation2.png"), size, size, true, true);
-		turtle3 = new Image(Main.class.getResourceAsStream("images/TurtleAnimation3.png"), size, size, true, true);
-		setX(xpos);
-		setY(ypos);
-		speed = s;
-		setImage(turtle2);
+	
+	public void changeTurtleImage(int index) {
+		setImage(turtleImages.get(index));
 	}
 }
