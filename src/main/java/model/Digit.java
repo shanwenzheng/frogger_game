f577845146829e4b6d195e4d48404837dd6293ce
scyws1 <scyws1@exmal.nottingham.ac.uk>
2019-11-27 22:36:44 +0000

    Refactor Digit class

--- src/main/java/model/Digit.java
+++ src/main/java/model/Digit.java
@@ -1,19 +1,10 @@
 package model;
 
-
-import frogger.Main;
 import javafx.scene.image.Image;
 
 public class Digit extends StaticActor{
-	int dim;
-	Image im1;
 
-	
-	public Digit(int n, int dim, int x, int y) {
-		im1 = new Image(Main.class.getResourceAsStream("images/"+n+".png"), dim, dim, true, true);
-		setImage(im1);
-		setX(x);
-		setY(y);
+	public Digit(int n, int dim, double  x, double  y) {
+		super("file:resources/images/"+n+".png", dim, x, y);
 	}
-	
 }
