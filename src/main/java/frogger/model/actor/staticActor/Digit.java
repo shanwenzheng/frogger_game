1b021ce4e71a88151176e045e668636ec999f64f
scyws1 <scyws1@exmal.nottingham.ac.uk>
2019-11-30 20:34:35 +0000

    Refactor digit class (Add size field)

--- src/main/java/frogger/model/actor/staticActor/Digit.java
+++ src/main/java/frogger/model/actor/staticActor/Digit.java
@@ -1,27 +1,20 @@
 package frogger.model.actor.staticActor;
 
-import java.util.ArrayList;
-
 import frogger.Main;
 import javafx.scene.image.Image;
 
 public class Digit extends StaticActor{
 	
-	private ArrayList<Image> digitImages;
+	private int size;
 	
 	public Digit(int n, int size, double  x, double  y) {
 		super(x, y);
-		digitImages = new ArrayList<Image>() {{
-			for(int i = 0; i < 10; i ++)
-			{
-				add(new Image(Main.class.getResourceAsStream("images/" + i + ".png"), size, size, true, true));
-			}
-		}};
-		setImage(digitImages.get(n));
+		this.size = size;
+		setImage(new Image(Main.class.getResourceAsStream("images/" + n + ".png"), size, size, true, true));
 	}
 	
 	public void changeScore(int n) {
-		setImage(digitImages.get(n));
+		setImage(new Image(Main.class.getResourceAsStream("images/" + n + ".png"), size, size, true, true));
 	}
 }
 
